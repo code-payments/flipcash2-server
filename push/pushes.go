709a15629f92cbd1f25ca449f70e3637434ab718
jeffyanta <jeff@flipcash.com>
2026-08-20 12:28:13 -0400

    Remove Dollars currency name from sell push

--- push/pushes.go
+++ push/pushes.go
@@ -97,7 +97,7 @@ func SendFlipcashCurrencyBoughtPush(ctx context.Context, pusher Pusher, user *co
 func SendFlipcashCurrencySoldPush(ctx context.Context, pusher Pusher, user *commonpb.UserId, mint *commonpb.PublicKey, currencyName string, region ocp_currency.Code, nativeAmount float64) error {
 	title := fmt.Sprintf("%s Successfully Converted", currencyName)
 	body := amountPrinter.Sprintf(
-		"%s of Dollars was added to your Flipcash wallet",
+		"%s was added to your Flipcash wallet",
 		localization.FormatFiat(defaultLocale, region, nativeAmount),
 	)
 	customPayload := &pushpb.Payload{

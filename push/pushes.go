2a6ef68246680b0555938b77411283f912fa331d
jeffyanta <jeff@flipcash.com>
2026-08-20 09:27:15 -0400

    Rename Bought and Sold in pushes to Converted (#168)

--- push/pushes.go
+++ push/pushes.go
@@ -76,7 +76,7 @@ func SendFlipcashCurrencyDepositedPush(ctx context.Context, pusher Pusher, user 
 }
 
 func SendFlipcashCurrencyBoughtPush(ctx context.Context, pusher Pusher, user *commonpb.UserId, mint *commonpb.PublicKey, currencyName string, region ocp_currency.Code, nativeAmount float64) error {
-	title := fmt.Sprintf("%s Successfully Purchased", currencyName)
+	title := fmt.Sprintf("%s Successfully Converted", currencyName)
 	body := amountPrinter.Sprintf(
 		"%s of %s was added to your Flipcash wallet",
 		localization.FormatFiat(defaultLocale, region, nativeAmount),
@@ -95,7 +95,7 @@ func SendFlipcashCurrencyBoughtPush(ctx context.Context, pusher Pusher, user *co
 }
 
 func SendFlipcashCurrencySoldPush(ctx context.Context, pusher Pusher, user *commonpb.UserId, mint *commonpb.PublicKey, currencyName string, region ocp_currency.Code, nativeAmount float64) error {
-	title := fmt.Sprintf("%s Successfully Sold", currencyName)
+	title := fmt.Sprintf("%s Successfully Converted", currencyName)
 	body := amountPrinter.Sprintf(
 		"%s of Dollars was added to your Flipcash wallet",
 		localization.FormatFiat(defaultLocale, region, nativeAmount),
